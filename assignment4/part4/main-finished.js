76a84b3f5a1d66168634c0f07b6e3f164db8c440
NickAcre <nicholas.acre@dcmail.ca>
2024-03-27 15:37:47 -0400

    in-class progress for part 4

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -17,13 +17,20 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY, exists) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.exists = exists;
+  }
+}
+
+class Ball extends Shape {
+
+  constructor(x, y, velX, velY, exists, color, size) {
+    super(x, y, velX, velY, exists);
     this.color = color;
     this.size = size;
   }
@@ -58,7 +65,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball)&& ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -67,6 +74,59 @@ class Ball {
           ball.color = this.color = randomRGB();
         }
       }
+    }
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y){
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+  }
+
+  setControls() {
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
     }
   }
 }
@@ -82,6 +142,7 @@ while (balls.length < 25) {
     random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
+    true,
     randomRGB(),
     size
   );
@@ -103,3 +164,20 @@ function loop() {
 }
 
 loop();
+
+let evilCircle = new EvilCircle(
+  random(0, width),
+  random(0, height)
+);
+
+evilCircle.setControls();
+
+//make the evil loop function function for the evil circle
+function evilloop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  requestAnimationFrame(loop);
+}
+
+//make the evil circle exist in our physical universe ecks dee
